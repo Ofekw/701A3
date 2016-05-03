2a16d8ca708bc10de0d1ca6a0a54655a1936c211
Ofek <ofekwit@gmail.com>
2016-05-03 12:26:21 +1200

    UML update 2

--- src/kalah/MankalaGameBoard.java
+++ src/kalah/MankalaGameBoard.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MankalaGameBoard implements Board {
-	private List<PlayerBoard> boards = new ArrayList<PlayerBoard>();
+	private List<PlayerBoard> playerBoards = new ArrayList<PlayerBoard>();
 	
 	public MankalaGameBoard(int boardCount){
 		for (int i = 1; i <= boardCount; i++){
 			PlayerBoard board = new MankalaPlayerBoard(new Player(i));
-			this.boards.add(board);
+			this.playerBoards.add(board);
 		}
 	}
 	
 	@Override
 	public List<PlayerBoard> getPlayerBoards() {
-		return boards;
+		return playerBoards;
 	}
 
 	@Override
 	public void setBoards(List<PlayerBoard> boards) {
-		this.boards = boards;
+		this.playerBoards = boards;
 	}
 
 	
